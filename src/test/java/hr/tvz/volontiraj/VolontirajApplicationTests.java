d2353e5a3e037ab0e89fd697676d8ffede45e3e5
Nikola Tucek <nikola.tucek@hotmail.com>
2025-06-24 00:30:58 +0200

    Test main method starts

--- src/test/java/hr/tvz/volontiraj/VolontirajApplicationTests.java
+++ src/test/java/hr/tvz/volontiraj/VolontirajApplicationTests.java
@@ -1,13 +1,33 @@
 package hr.tvz.volontiraj;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class VolontirajApplicationTests {
 
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void main_shouldRunWithoutExceptions() {
+        VolontirajApplication.main(new String[] {});
+    }
+
     @Test
     void contextLoads() {
+        assertNotNull(context);
+    }
+
+    @Test
+    void restTemplateBeanExists() {
+        RestTemplate restTemplate = context.getBean(RestTemplate.class);
+        assertNotNull(restTemplate);
     }
 
 }
